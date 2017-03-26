e6e54071a78b09e66d5f2b81b6cab9441c2d27dd
maitmansour <mohamedaitmansour95@gmail.com>
2017-03-26 14:35:06 +0200

    MERYEM : Corrections de quelques bugs + ajout de system d'affichage de notes comme étoiles

---
+++ src/AppBundle/Controller/CommentaireController.php
@@ -0,0 +1,23 @@
+<?php
+// src/AppBundle/Controller/MuseeController.php
+
+namespace AppBundle\Controller;
+use AppBundle\Form\MuseeType;
+use AppBundle\Entity\Commentaire;
+use AppBundle\Entity\Musee;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+
+class MuseeController extends Controller
+{
+
+
+}
